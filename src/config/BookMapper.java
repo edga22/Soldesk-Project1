aeadcbb3871f446908a34b3b2d8fdbacb76db909
angelmaster <yoo5459@gmail.com>
2017-03-29 09:52:17 +0900

    유강현 맵퍼 문제 해결

--- src/config/BookMapper.java
+++ src/config/BookMapper.java
@@ -6,9 +6,9 @@ import domain.Book;
 public interface BookMapper {
 	List<Book> getBooks();
 	Book getBook(int bookID);
+	Book getTitleBook(String title);	
+	
 	int cntBooks();
 	int cntBook(int bookID);
-	int addBook(Book book);
-	
-	Book getTitleBook(String title);
+	int addBook(Book book);	
 }
