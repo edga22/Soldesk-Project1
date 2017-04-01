6d03880b0245ece0f3336274bf252174dc047dc0
DESKTOP-FHG2IGG\박원서 <pdords@naver.com>
2017-04-01 14:01:42 +0900

    박원서 맴버 서비스 생성

---
+++ src/mgr/MemberService.java
@@ -0,0 +1,37 @@
+package mgr;
+
+import config.Factory;
+import config.MemberMapper;
+import domain.Member;
+
+public class MemberService{
+	MemberMapper member;
+	
+	public MemberService(){
+		member = Factory.getMapper(MemberMapper.class);
+	}
+	
+	public Member[] getMembers(){
+		return member.getMembers().toArray(new Member[member.getMembers().size()]);
+	}
+	
+	public Member getMember(int memberID){
+		return member.getMember(memberID);
+	}
+	
+	public int addMember(int MemberID){
+		return member.addMember(MemberID);
+	}
+	
+	public int findIdMember(Member Email){
+		return member.findIdMember(Email);
+	}
+	
+	public int findPwMember(Member psw){
+		return member.findPwMember(psw);
+	}
+	
+	public int delMember(int MemberID){
+		return member.delMember(MemberID);
+	}
+}
