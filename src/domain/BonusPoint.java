89b0caed141a5d452b79804e781209b59b2c050f
DESKTOP-6KC3ED4\GwangJae <dbseoen@naver.com>
2017-04-06 16:26:23 +0900

    윤광재 2017.04.07. BonusPoint 도메인 작성중!

---
+++ src/domain/BonusPoint.java
@@ -0,0 +1,19 @@
+package domain;
+
+import domain.Member;
+
+public class BonusPoint extends Member{
+	private int BonusCnt;
+	
+	Member member = new Member();
+	
+	
+	public int AddBonusPoint(){
+		
+		return BonusCnt;
+	}
+	
+	
+	
+	
+}
