e3a884427a4cffa737aa37866de85421f1aaab41
DESKTOP-6KC3ED4\GwangJae <dbseoen@naver.com>
2017-04-07 10:20:02 +0900

    윤광재 2017.04.07 포인트 도메인 및 DAO (베타 테스트 001)

---
+++ src/dao/BonusPointDao.java
@@ -0,0 +1,7 @@
+package dao;
+
+import domain.Member;
+
+public interface BonusPointDao {
+	int findBonusPoint(Member bonusPoint);
+}
--- src/domain/BonusPoint.java
+++ src/domain/BonusPoint.java
@@ -1,17 +1,26 @@
 package domain;
 
 import domain.Member;
+import domain.Book;
 
-public class BonusPoint extends Member{
-	private int BonusCnt;
-	
+public class BonusPoint{
 	Member member = new Member();
+	Book book = new Book();
+	
+	private int price = book.getPrice();
+	private int bonusPoint = member.getBonusPoint();	
+	
+	public BonusPoint(){}	
 	
 	
-	public int AddBonusPoint(){
+	/*public BonusPoint(int price, int bonusPoint){
+		this.price = book.getPrice();
+		this.bonusPoint = member.getBonusPoint();
 		
-		return BonusCnt;
-	}
+		bonusPoint = (int)(price * 0.1);		
+	}*/
+	
+	
 	
 	
 	
