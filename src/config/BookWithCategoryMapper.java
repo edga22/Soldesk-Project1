0948ba31a3e6c0decdf385594c570b9121007838
soldesk-PC\soldesk <chungwa2@nate.com>
2017-04-18 18:53:44 +0900

    정재흠 분류페이지 도서리스트

--- src/config/BookWithCategoryMapper.java
+++ src/config/BookWithCategoryMapper.java
@@ -7,8 +7,6 @@ import domain.BookWithCategory;
 
 public interface BookWithCategoryMapper {
 	List<BookWithCategory> getAllBookWithCategory();
-	List<BookWithCategory> getBookWithCategorycategory(int categoryID);
-	List<BookWithCategory> getBookWithCategorycode1(int code1);
-	List<BookWithCategory> getBookWithCategoryBooks(Book target);
-	BookWithCategory getBookWithCategoryBook(int BookID);
+	List<BookWithCategory> getBookWithCategoryBooks(int[] bookids);
+	List<BookWithCategory> getBookWithCategoryBook(Book book);
 }
